666248e10edcb5a880dca400ea4bb8e7562af68f
zikaiqin <zikaiqin99@gmail.com>
2023-06-05 01:26:29 -0400

    added chat name inputs

--- TP1/app/src/scripts/chat.js
+++ TP1/app/src/scripts/chat.js
@@ -1,1 +1,35 @@
-// TODO : insert new chat elements
+$(document).ready(() => {
+  $('#name-add').click(() => {
+    showForm();
+  });
+
+  $('#name-confirm').click(() => {
+    let form = $('#name-form');
+    if (!form[0].checkValidity()) {
+      form.addClass('was-validated');
+      return;
+    }
+    hideForm();
+  });
+
+  $('#name-reset').click(() => {
+    $('#name-form').removeClass('was-validated');
+    $('#name-input').focus();
+  });
+
+  $('#name-cancel').click(() => {
+    hideForm();
+  });
+
+});
+
+function showForm () {
+  $('#name-add').hide();
+  $('#name-form').show();
+  $('#name-input').focus();
+}
+
+function hideForm () {
+  $('#name-add').show();
+  $('#name-form').hide()[0].reset();
+}
